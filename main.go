efc5c05cde2a00df211e639a2d16b7e98bef87cd
Dmitry Savintsev <dsavints@gmail.com>
2016-02-16 14:24:28 +0100

    reduce deps to clarify the point

--- main.go
+++ main.go
@@ -2,26 +2,11 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
 
 	"github.com/dmitris/deptesthuge/deptestsmall"
-	"golang.org/x/net/html"
 )
 
 func main() {
 	s := deptestsmall.SillyType{}.String()
 	fmt.Printf("[INFO]: %s\n", s)
-
-	res, err := http.Get("http://en.wikipedia.org")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Parse the HTML
-	doc, err := html.Parse(res.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("Doc: #%v\n", doc)
 }
