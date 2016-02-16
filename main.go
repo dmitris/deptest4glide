aa7b893e39456a9585dfb61d74277bef3ccce0e7
Dmitry Savintsev <dsavints@yahoo-inc.com>
2016-02-16 13:48:28 +0100

    add github.com/dmitris/deptesthuge as dependency

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/dmitris/deptesthuge/deptestsmall"
 	"golang.org/x/net/html"
 )
 
 func main() {
+	s := deptestsmall.SillyType{}.String()
+	fmt.Printf("[INFO]: %s\n", s)
+
 	res, err := http.Get("http://en.wikipedia.org")
 	if err != nil {
 		log.Fatal(err)
